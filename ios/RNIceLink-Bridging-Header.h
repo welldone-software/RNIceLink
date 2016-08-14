4e5eb23232d9ba12fe835344c01ad20f29537b0a
Lior Shalev <liorshalev01@gmail.com>
2016-08-14 16:05:45 +0300

    working

--- ios/RNIceLink-Bridging-Header.h
+++ ios/RNIceLink-Bridging-Header.h
@@ -1,4 +1,3 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
+#import "RCTRootView.h"
+#import "RCTViewManager.h"
+#import "RCTBridgeModule.h"
