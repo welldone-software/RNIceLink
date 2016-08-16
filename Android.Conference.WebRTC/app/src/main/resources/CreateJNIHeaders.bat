423de9e928a953b3f434bafc895dde60dec31ccb
Lior Shalev <liorshalev01@gmail.com>
2016-08-16 13:15:03 +0300

    updates

--- Android.Conference.WebRTC/app/src/main/resources/CreateJNIHeaders.bat
+++
@@ -1,9 +0,0 @@
-del aopus_OpusLibrary.h
-javac aopus/OpusLibrary.java
-javah aopus.OpusLibrary
-del aopus\OpusLibrary.class
-
-del aaudioprocessing_AudioProcessingLibrary.h
-javac aaudioprocessing/AudioProcessingLibrary.java
-javah aaudioprocessing.AudioProcessingLibrary
-del aaudioprocessing\AudioProcessingLibrary.class
